6d0125dfb4d535d66095dd38df2d6e4f4558ced4
ElmarsMozulis <125596330+ElmarsMozulis@users.noreply.github.com>
2023-03-19 21:38:40 +0000

    def p_works(m, data):     jobs = []     for i in range(m):         job = [0, i, data[i]]         jobs.append(job)     return jobs
    def p_threads(n):
        threads = []
        for i in range(n):
            thread = [0, i]
            threads.append(thread)
        return threads
    
    def parallel_processing(n, m, data):
        jobs = p_works(m, data)
        threads = p_threads(n)
        output = []
    
        for job in jobs:
            mini_thread = threads[0]
            min_t_id = 0
            for i in range(len(threads)):
                if threads[i][0] < mini_thread[0]:
                    mini_thread = threads[i]
                    min_t_id = i
    
            start_time = max(mini_thread[0], job[0])
            output.append((mini_thread[1], start_time))
            threads[min_t_id][0] = start_time + job[2]
    
        return output
    
    def main():
        n, m = map(int, input().split())
        data = list(map(int, input().split()))
    
        result = parallel_processing(n, m, data)
    
        for thread, start_time in result:
            print(thread, start_time)
    
    if __name__ == "__main__":
        main()

--- main.py
+++ main.py
@@ -1,31 +1,45 @@
-# python3
+def p_works(m, data):
+    jobs = []
+    for i in range(m):
+        job = [0, i, data[i]]
+        jobs.append(job)
+    return jobs
+
+def p_threads(n):
+    threads = []
+    for i in range(n):
+        thread = [0, i]
+        threads.append(thread)
+    return threads
 
 def parallel_processing(n, m, data):
+    jobs = p_works(m, data)
+    threads = p_threads(n)
     output = []
-    # TODO: write the function for simulating parallel tasks, 
-    # create the output pairs
+
+
+    for job in jobs:
+        mini_thread = threads[0]
+        min_t_id = 0
+        for i in range(len(threads)):
+            if threads[i][0] < mini_thread[0]:
+                mini_thread = threads[i]
+                min_t_id = i
+
+        start_time = max(mini_thread[0], job[0])
+        output.append((mini_thread[1], start_time))
+        threads[min_t_id][0] = start_time + job[2]
 
     return output
 
 def main():
-    # TODO: create input from keyboard
-    # input consists of two lines
-    # first line - n and m
-    # n - thread count 
-    # m - job count
-    n = 0
-    m = 0
+    n, m = map(int, input().split())
+    data = list(map(int, input().split()))
 
-    # second line - data 
-    # data - contains m integers t(i) - the times in seconds it takes any thread to process i-th job
-    data = []
+    result = parallel_processing(n, m, data)
 
-    # TODO: create the function
-    result = parallel_processing(n,m,data)
-    
-    # TODO: print out the results, each pair in it's own line
-
-
+    for thread, start_time in result:
+        print(thread, start_time)
 
 if __name__ == "__main__":
     main()
