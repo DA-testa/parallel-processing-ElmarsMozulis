0519f0ac34de106f8546086368333a7ce92738e3
ElmarsMozulis <125596330+ElmarsMozulis@users.noreply.github.com>
2023-03-19 22:06:47 +0000

    def parallel_processing(int_n, int_m, data):     parallel_threads = [(0, i) for i in range(int_n)]     results = []
        for i in range(int_m):
            job_time = data[i]
    
            start_time, p_thread_id = parallel_threads[0]
            results.append((p_thread_id, start_time))
    
            parallel_threads[0] = (start_time + job_time, p_thread_id)
            parallel_threads.sort()
    
        return results
    
    def main():
        int_n, int_m = map(int, input().split())
        data = list(map(int, input().split()))
    
        result = parallel_processing(int_n, int_m, data)
        for thread_id, start_time in result:
            print(thread_id, start_time)
    
    if __name__ == "__main__":
        main()

--- main.py
+++ main.py
@@ -1,25 +1,25 @@
-def parallel_processing(n, m, data):
-    threads = [(0, i) for i in range(n)] 
+def parallel_processing(int_n, int_m, data):
+    parallel_threads = [(0, i) for i in range(int_n)] 
     results = [] 
 
-    for i in range(m):
+    for i in range(int_m):
         job_time = data[i]
 
     
-        start_time, thread_id = threads[0]
-        results.append((thread_id, start_time)) 
+        start_time, p_thread_id = parallel_threads[0]
+        results.append((p_thread_id, start_time)) 
 
-        threads[0] = (start_time + job_time, thread_id)
-        threads.sort()
+        parallel_threads[0] = (start_time + job_time, p_thread_id)
+        parallel_threads.sort()
 
     return results
 
 
 def main():
-    n, m = map(int, input().split())
+    int_n, int_m = map(int, input().split())
     data = list(map(int, input().split()))
 
-    result = parallel_processing(n, m, data)
+    result = parallel_processing(int_n, int_m, data)
     for thread_id, start_time in result:
         print(thread_id, start_time)
 
